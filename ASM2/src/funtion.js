7aca33429cab83211a0c9ed8e1b31ffb7509d245
nptin1996 <137751006+nptin1996@users.noreply.github.com>
2024-04-13 14:43:28 +0700

    Delete ASM2 directory

--- ASM2/src/funtion.js
+++
@@ -1,7 +0,0 @@
-export function truncateString(str, num = 200) {
-  return str.length > num ? str.substring(0, num) + "..." : str;
-}
-
-export function getRandomElement(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
